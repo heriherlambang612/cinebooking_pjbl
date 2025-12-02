ff27e26dda6cad907f1f3ba94d280e28aa2ecad6
Nicko Sugiarto <nicko9995sgt@gmail.com>
2025-12-02 16:42:16 +0700

    seat

---
+++ lib/widgets/seat_item_nick.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SeatItem_nick extends StatelessWidget {
+  final String seatCode;
+  final bool isSelected;
+  final bool isSold;
+  final VoidCallback onTap;
+
+  SeatItem_nick({required this.seatCode, required this.isSelected, required this.isSold, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    Color seatColor = Colors.grey;
+    if (isSold) {
+      seatColor = Colors.red;
+    } else if (isSelected) {
+      seatColor = Colors.blue;
+    }
+
+    return GestureDetector(
+      onTap: isSold ? null : onTap,
+      child: Container(
+        decoration: BoxDecoration(color: seatColor, borderRadius: BorderRadius.circular(5)),
+        child: Center(
+          child: Text(
+            seatCode,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
